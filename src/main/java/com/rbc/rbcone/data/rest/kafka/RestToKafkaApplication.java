1a3a4cc08f8ed8621a63759fdc3fc1fd0dc953c4
Pedro Marce Rodriguez <pedromarcerodriguez@Pedros-MacBook.local>
2018-01-09 14:09:05 +0100

    Fix gitignore

--- src/main/java/com/rbc/rbcone/data/rest/kafka/RestToKafkaApplication.java
+++ src/main/java/com/rbc/rbcone/data/rest/kafka/RestToKafkaApplication.java
@@ -1,7 +1,14 @@
 package com.rbc.rbcone.data.rest.kafka;
 
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 import org.apache.http.HttpHost;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -10,25 +17,38 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.InetAddress;
 
 @SpringBootApplication
 public class RestToKafkaApplication {
 
-    @Bean(name="KStreamBuilder")
-    public KStreamBuilder kStreamBuilder() {
-        return new KStreamBuilder();
+    @Bean(name="StreamsBuilder")
+    public StreamsBuilder streamsBuilder() {
+        return new StreamsBuilder();
     }
-
 
     @Bean
     public RestHighLevelClient restHighLevelClient(@Value("${elasticsearch.server.url}") String serverUrl,
-                                                   @Value("${elasticsearch.port}") Integer port) throws Exception {
-        RestClient client = RestClient.builder(
-                new HttpHost(InetAddress.getByName(serverUrl), port)).build();
-
-        return new RestHighLevelClient(client);
+                                                   @Value("${elasticsearch.port}") Integer port,
+                                                   @Value("${elasticsearch.protocol}") String protocol) throws Exception {
+        return new RestHighLevelClient(RestClient.builder(
+                new HttpHost(serverUrl, port, protocol)));
     }
+
+    @Bean
+    public Firestore firestore() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("src/main/resources/Hackathon.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://hackathon-60430.firebaseio.com")
+                .build();
+
+        return FirestoreClient.getFirestore(FirebaseApp.initializeApp(options));
+    }
+
 
     public static void main(final String[] args) {
 		SpringApplication.run(RestToKafkaApplication.class, args);
